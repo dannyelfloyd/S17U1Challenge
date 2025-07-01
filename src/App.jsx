6067b76ba7ac1ff48119933e63bb66b772b605c8
dannyelfloyd <dannyelfloyd@gmail.com>
2025-07-01 09:08:17 +0200

    Entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import './App.css'
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
  //Aquí tu código
-}
+  const [name, setName] = useState("Sofía");
+  const [newName, setNewName] = useState('');
 
-export default App
+  return (
+    <div>
+      <h2>Teacher Name: {name}</h2>
+      <ul>
+        <li onClick={()=>setName("Data")}>Data</li>
+        <li onClick={()=>setName("Reyes")}>Reyes</li>
+        <li onClick={()=>setName("Yolanda")}>Yolanda</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
